86b5c8310b96855c03bb85d1f95777c5cec1944f
harveymanalili <harveymanalililili@gmail.com>
2022-12-20 20:21:48 +0000

    Responsive Navbar

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let parallaxbg = document.getElementById("aboutmebg");
+const hamburger_button = document.getElementsByClassName("hamburger-menu")[0];
+const navlinks = document.getElementsByClassName("navlinks")[0];
+
 
 window.addEventListener("scroll",function(){
     var value = window.scrollY;
     parallaxbg.style.top = value * 0.965 + 'px';
+})
+
+
+hamburger_button.addEventListener("click",() => {
+    navlinks.classList.toggle("active");
+ 
 })
